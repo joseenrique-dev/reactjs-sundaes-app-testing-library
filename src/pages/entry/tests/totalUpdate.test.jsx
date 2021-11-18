36efc30d43ae3d261ebc09af7c0e75a31917f323
José Enrique <otipip90@gmail.com>
2021-11-18 02:13:52 -0500

    created test -test('update scoop subtotal when scoop change', async () => {-

--- src/pages/entry/tests/totalUpdate.test.jsx
+++ src/pages/entry/tests/totalUpdate.test.jsx
@@ -26,3 +26,28 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update scoop subtotal when scoop change', async () => {
+  render(<Options optionType='scoops' />, { wrapper: OrderDetailsProvider });
+
+  //make sure total start al $0.00
+  const scoopsSubtotal = screen.getByText('Scoops total: $0.00');
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  //update vanilla scoop to 1 and check the subtotal
+  const vanillaScoop = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaScoop);
+  userEvent.type(vanillaScoop, '1');
+
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
+
+  //update chocolate scoop to 2 and check subtotal
+  const chocolateScoop = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+  userEvent.clear(chocolateScoop);
+  userEvent.type(chocolateScoop, '2');
+  expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
